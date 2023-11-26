3e7d5118a9baddad6c413d5b7848ac1a1b362ab0
04anilr <anilr5364@gmail.com>
2023-11-26 16:40:22 +0530

    feat

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header"
+import Main from "@/components/main"
+import Footer from "@/components/Footer"
+export default function Home() {
+  return (
+   <>
+   <Header />
+   <Main />
+   <Footer />
+ 
+   </>
+  )
+}
