c231864fec957d879b578b9441d969257c90602b
04anilr <anilr5364@gmail.com>
2023-11-28 11:22:28 +0530

    update 84 line

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -81,7 +81,7 @@ return (
      onClick={(e) => onSearchSubmit(e)}>Google Search</button>
     <button 
     className="bg-slate-100  py-2 px-4 text-sm rounded hover:border"
-    onClick={() => router.push('https://doodles.google/')}>I'm Feeling Lucky</button>
+    onClick={() => router.push('https://doodles.google/')}>I&apos;m Feeling Lucky</button>
   </div>
   </div>
 )
